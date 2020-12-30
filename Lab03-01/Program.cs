3b1ebde61aff93fc75d2d87c87b38f5bbde54b80
Nguyễn Thanh Tú <ntt11052000@gmail.com>
2020-12-30 21:55:32 +0700

    Progress: 35% ex1, 0% ex2, 0% ex3

--- Lab03-01/Program.cs
+++ Lab03-01/Program.cs
@@ -16,7 +16,7 @@ namespace Lab03_01
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmQLSV());
         }
     }
 }
